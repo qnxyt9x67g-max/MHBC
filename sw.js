22ff3640e12f2ba9472e80eead5bd555b4ac9b70
qnxyt9x67g-max <dcobb1989@gmail.com>
2026-06-22 06:30:09 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 // MHBC Service Worker — caching + background notifications
-const CACHE = 'mhbc-67';
+const CACHE = 'mhbc-68';
 
 const ASSETS = [
   './',
